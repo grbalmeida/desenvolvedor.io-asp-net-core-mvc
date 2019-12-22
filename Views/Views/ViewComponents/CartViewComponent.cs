a592c5f66b307fb3d5ad2334ef9ac85ff7cac81b
gilvan <g.r.almeida@live.com>
2019-12-22 18:12:14 -0300

    My first view component

---
+++ Views/Views/ViewComponents/CartViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Views.ViewComponents
+{
+    [ViewComponent(Name = "Cart")]
+    public class CartViewComponent : ViewComponent
+    {
+        public int ItemsInCart { get; set; }
+
+        public CartViewComponent()
+        {
+            ItemsInCart = 3;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(ItemsInCart);
+        }
+    }
+}
