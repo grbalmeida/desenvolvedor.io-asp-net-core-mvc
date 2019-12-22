b176579ea90936a694339f6babae2db224b0e89e
gilvan <g.r.almeida@live.com>
2019-12-21 22:49:34 -0300

    First app

---
+++ MyMvcDemo/MyMvcDemo/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MyMvcDemo.Models;
+
+namespace MyMvcDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(string id, string category)
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
