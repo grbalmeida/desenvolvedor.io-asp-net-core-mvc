cb92ec1beebfe4816711193997ebf8507d0e2f96
gilvan <g.r.almeida@live.com>
2019-12-22 18:42:58 -0300

    Working with forms

---
+++ Views/Views/Controllers/MoviesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Views.Controllers
+{
+    public class MoviesController : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
