49d8ef9b4087961b53e3918c4c6dad7e25db8fb7
gilvan <g.r.almeida@live.com>
2019-12-22 01:10:25 -0300

    Working with models

---
+++ Models/Models/Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Models.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+        public int Rate { get; set; }
+    }
+}
