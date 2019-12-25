c4d8f9dc63cc0771f1becef4d67fc27a52034bbc
gilvan <g.r.almeida@live.com>
2019-12-24 21:05:15 -0300

    Injecting dependencies into MVC

--- ModelApp/src/DevIO.ModelApp/Controllers/HomeController.cs
+++ ModelApp/src/DevIO.ModelApp/Controllers/HomeController.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DevIO.ModelApp.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DevIO.ModelApp.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly IOrderRepository _orderRepository;
+
+        public HomeController(IOrderRepository orderRepository)
         {
+            _orderRepository = orderRepository;
+        }
+
+        public IActionResult Index([FromServices] IOrderRepository _orderRepository2)
+        {
+            var order = _orderRepository.GetOrder();
+            var order2 = _orderRepository2.GetOrder();
             return View();
         }
     }
