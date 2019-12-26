b02b5a408d8e66dadd994144321399f646aba8a4
gilvan <g.r.almeida@live.com>
2019-12-26 20:59:43 -0300

    Authentication

--- AspNetCoreIdentity/AspNetCoreIdentity/Controllers/HomeController.cs
+++ AspNetCoreIdentity/AspNetCoreIdentity/Controllers/HomeController.cs
@@ -5,16 +5,20 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using AspNetCoreIdentity.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace AspNetCoreIdentity.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
+        [AllowAnonymous]
         public IActionResult Index()
         {
             return View();
         }
 
+        [Route("about")]
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
@@ -22,6 +26,7 @@ namespace AspNetCoreIdentity.Controllers
             return View();
         }
 
+        [Route("contact")]
         public IActionResult Contact()
         {
             ViewData["Message"] = "Your contact page.";
@@ -29,6 +34,7 @@ namespace AspNetCoreIdentity.Controllers
             return View();
         }
 
+        [Route("privacy")]
         public IActionResult Privacy()
         {
             return View();
