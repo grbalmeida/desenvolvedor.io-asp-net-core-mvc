164e8c8bdd442d841f70db27fdfb4690fcfea1f6
gilvan <g.r.almeida@live.com>
2019-12-28 22:48:42 -0300

    Protecting data with user secrets

--- AspNetCoreIdentity/AspNetCoreIdentity/Startup.cs
+++ AspNetCoreIdentity/AspNetCoreIdentity/Startup.cs
@@ -19,6 +19,11 @@ namespace AspNetCoreIdentity
                 .AddJsonFile($"appsettings.{hostingEnvironment.EnvironmentName}.json", true, true)
                 .AddEnvironmentVariables();
 
+            if (hostingEnvironment.IsProduction())
+            {
+                builder.AddUserSecrets<Startup>();
+            }
+
             Configuration = builder.Build();
         }
 
