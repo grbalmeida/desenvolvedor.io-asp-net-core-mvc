24bcfa268c7573e953e0abad86c7b58194d792a9
gilvan <g.r.almeida@live.com>
2020-01-05 22:15:41 -0300

    Using ajax for postal code filling

--- MyFullMvcApp/src/DevIO.App/wwwroot/js/site.js
+++ MyFullMvcApp/src/DevIO.App/wwwroot/js/site.js
@@ -38,3 +38,55 @@
         }
     });
 }
+
+function SearchPostalCode() {
+    $(document).ready(function () {
+
+        function clearPostalCodeForm() {
+            $('#Address_Street').val('');
+            $('#Address_District').val('');
+            $('#Address_City').val('');
+            $('#Address_State').val('');
+        }
+
+        $('#Address_PostalCode').blur(function () {
+
+            var postalCode = $(this).val().replace(/\D/g, '');
+
+            if (postalCode != '') {
+
+                var validatePostalCode = /^[0-9]{8}$/;
+
+                if (validatePostalCode.test(postalCode)) {
+
+                    $('#Address_Street').val('...');
+                    $('#Address_District').val('...');
+                    $('#Address_City').val('...');
+                    $('#Address_State').val('...');
+
+                    $.getJSON('https://viacep.com.br/ws/' + postalCode + '/json/?callback=?',
+                        function (data) {
+
+                            if (!('erro' in data)) {
+                                $('#Address_Street').val(data.logradouro);
+                                $('#Address_District').val(data.bairro);
+                                $('#Address_City').val(data.localidade);
+                                $('#Address_State').val(data.uf);
+                            }
+                            else {
+                                clearPostalCodeForm();
+                                alert('Postal Code not found.');
+                            }
+                        });
+                }
+                else {
+                    clearPostalCodeForm();
+                    alert('Postal Code in invalid format.');
+                }
+            }
+            else {
+                clearPostalCodeForm();
+            }
+        });
+    });
+}
